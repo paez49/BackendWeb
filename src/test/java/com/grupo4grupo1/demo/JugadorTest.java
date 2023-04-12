3cc90f85685a73340626a306cbddad4a176b3db5
Juan Páez <jdpluc302@gmail.com>
2023-04-11 23:41:05 -0500

    Probando pruebas unitarias

--- src/test/java/com/grupo4grupo1/demo/JugadorTest.java
+++ src/test/java/com/grupo4grupo1/demo/JugadorTest.java
@@ -1,14 +1,34 @@
 package com.grupo4grupo1.demo;
 
+import com.grupo4grupo1.demo.model.Usuario;
 import com.grupo4grupo1.demo.repository.UsuarioRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class JugadorTest {
 
   @Autowired
   UsuarioRepository userRepository;
+  @Test
+  public void pruebaInsercionUsuario(){
+    try{
+      Usuario usuario = new Usuario();
 
+      usuario.setNombre("Juan");
+      usuario.setApellido("Paez");
+      usuario.setCorreo("juan@paez.com");
+
+      userRepository.save(usuario);
+    }
+    catch (Exception e){
+      e.printStackTrace();
+      assertTrue( false, "Error en la aplicación" );
+
+    }
+  }
 }
