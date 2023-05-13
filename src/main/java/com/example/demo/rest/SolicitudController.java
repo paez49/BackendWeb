0d880cc187accf153c25cf2cb3d6fcf18f2d0683
Juan Páez <jdpluc302@gmail.com>
2023-05-13 12:26:17 -0500

    Ajuste ruta crear solicitud

--- src/main/java/com/example/demo/rest/SolicitudController.java
+++ src/main/java/com/example/demo/rest/SolicitudController.java
@@ -23,8 +23,8 @@ public class SolicitudController {
 
     //Crear solicitud
     //Lista solicitudes -> Boton enviar solicitud
-    @PostMapping("/add/{idUsuario}/{idEquipo}")
-    public ResponseEntity<?> crearSolicitud(@PathVariable Long idUsuario, @PathVariable Long idEquipo) {
+    @PostMapping("/add")
+    public ResponseEntity<?> crearSolicitud(@RequestBody Long idUsuario, @RequestBody Long idEquipo) {
         try {
             solicitudService.crearSolicitud(idUsuario, idEquipo);
             return ResponseEntity.ok(new MessageResponse("Solicitud enviada con exito"));
