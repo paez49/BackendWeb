f3d250a826e20e9896e77a19a8c5c410a939c86d
Juan Páez <jdpluc302@gmail.com>
2023-05-29 21:19:13 -0500

    Actualizado prueba crear equipo
    Estaba mal hecha

--- src/test/java/com/example/demo/controller/EquipoControllerTest.java
+++ src/test/java/com/example/demo/controller/EquipoControllerTest.java
@@ -16,6 +16,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 
@@ -39,9 +40,9 @@ public class EquipoControllerTest {
     }
     @Test
     public void testAgregarEquipo() throws Exception {
-        mockMvc.perform(post("/equipos/create")
-                .contentType("application/json")
-                .content("{\"nombreEquipo\":\"Equipo 1\",\"siglas\":\"EQ1\"}"))
+        mockMvc.perform(post("/equipos/create/{idUsuario}", 4L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"nombreEquipo\":\"Equipo 1\",\"siglas\":\"EQ1\"}"))
                 .andExpect(status().isCreated());
     }
     @Test
